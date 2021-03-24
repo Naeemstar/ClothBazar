fdc517bb7b0e3d3f1709e6505a29a3fcf27e6fbb
Naeemullah <unaeem281@gmail.m>
2021-03-24 16:19:42 +0500

    added category and product bussiness entityes and base entities

---
+++ ClothBazar/ClothBazar.Database/Migrations/202103241112161_tabladded.cs
@@ -0,0 +1,44 @@
+﻿namespace ClothBazar.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class tabladded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.categories",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.products",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Name = c.String(),
+                        Description = c.String(),
+                        category_ID = c.Int(),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.categories", t => t.category_ID)
+                .Index(t => t.category_ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.products", "category_ID", "dbo.categories");
+            DropIndex("dbo.products", new[] { "category_ID" });
+            DropTable("dbo.products");
+            DropTable("dbo.categories");
+        }
+    }
+}
---
+++ ClothBazar/ClothBazar.Entities/product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entities
+{
+   public class product: BaseEntities
+    {
+        
+        public decimal price { get; set; }
+        public category category { get; set; }
+    }
+}
