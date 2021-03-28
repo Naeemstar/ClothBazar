82b6798ce57ce1ae6073462aeb57c0ea509a61ab
Naeemullah <unaeem281@gmail.m>
2021-03-28 12:04:21 +0500

    search operation using ajax

---
+++ ClothBazar/ClothBazar.services/productservices.cs
@@ -0,0 +1,63 @@
+﻿using ClothBazar.Database;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.services
+{
+   public class productservices
+    {
+        public void saveproduct(product product)
+        {
+            using (var context = new CBContext())
+            {
+                context.products.Add(product);
+                context.SaveChanges();
+            }
+        }
+        public List<product> getproduct()
+        {
+            using (var context = new CBContext())
+            {
+                 return context.products.ToList();
+               
+            }
+        }
+        public product getsangleproduct(int id)
+        { 
+            using (var context = new CBContext())
+            {
+                return context.products.Find(id);
+
+            }
+        }
+        public void updateproduct(product product)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        //public category getdeletelecategory(int id)
+        //{
+        //    using (var context = new CBContext())
+        //    {
+        //        return context.categories.Find(id);
+
+        //    }
+        //}
+        public void deleteproduct(product product)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
--- ClothBazar/ClothBazar.web/Controllers/productController.cs
+++ ClothBazar/ClothBazar.web/Controllers/productController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ClothBazar.Entities;
+using ClothBazar.services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,32 @@ namespace ClothBazar.web.Controllers
 {
     public class productController : Controller
     {
+        productservices productservic = new productservices();
         // GET: product
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult producttable(string search)
+        {
+            var products = productservic.getproduct();
+            if(string.IsNullOrEmpty(search)==false)
+            {
+                products = products.Where(p => p.Name.Contains(search)).ToList();
+            }
+           
+            return PartialView(products);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Create(product product)
+        {
+            productservic.saveproduct(product);
+            return RedirectToAction("producttable");
         }
     }
 }
